ba13dcf92723a5866875ddf291876da1365423e6
Tiago Vieira <tvieira_92@hotmail.com>
2023-02-23 15:16:38 +0000

    Novas alterações

--- superstore_dw/models/stg_employee.sql
+++ superstore_dw/models/stg_employee.sql
@@ -1,6 +1,6 @@
 select
+    {{dbt_utils.generate_surrogate_key(['e.employee_id'])}} as sk_employee, -- adicionar a coluna surrogate key, utilizei uma macro do dbt (necessários instalar um package)
     e.employee_id,
     e.name as employee_name,
-    r.region
+    now() as created_at -- registo de quando uma dada linha é criada 
 from {{ source("norm", "t_employee") }} e
-    join {{ source("norm", "t_region") }} r on e.region_id = r.region_id
